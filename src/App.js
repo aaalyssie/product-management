6aa834f682530cc50188619b82d370cd5d43a18c
caluyaalyssamae <caluyaalyssamae@gmail.com>
2021-02-04 00:44:13 +0800

    initial product

--- src/App.js
+++ src/App.js
@@ -6,8 +6,26 @@ import {EditProduct} from './components/EditProduct';
 import {GlobalProvider} from './context/GlobalState';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {v4 as uuid} from 'uuid';
 
 function App() {
+    if (localStorage.getItem("Products") === null) {
+        let today = new Date();
+        let date = parseInt(today.getMonth()+1) + "/"+ today.getDate() +"/"+today.getFullYear();
+        const initialState = {
+            products: [{
+                id: uuid(),
+                date: date,
+                name: 'My Product',
+                description: 'The first product',
+                imageUrl: 'https://i.imgur.com/vpaB6oB.png',
+                featured: 'featured'
+                }
+            ]
+        }
+        localStorage.setItem('Products', JSON.stringify(initialState));
+    }
+    
     return (
       <div className="App">
           <GlobalProvider>
